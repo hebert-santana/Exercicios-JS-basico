51c0b79ec524c932b55acc90bd15dbd8a09bd661
Hebert Santana <102166830+hebert-santana@users.noreply.github.com>
2022-07-31 00:18:39 -0300

    add churrascometro

---
+++ Churrascometro/README.md
@@ -0,0 +1,27 @@
+# Churrascometro (Programador BR)
+## Projeto (finalizado :white_check_mark:)
+
+Projeto desenvolvido durante curso de <strong><em>"Curso de Desenvolvimento WEB FULL STACK"</em></strong> do curso <strong>Programador BR</strong>.
+
+## 🛠️ Recursos
+
+* HTML
+* CSS
+* Javascript
+
+## 💻 Demonstração
+ <p align="center">
+ <img align="center" alt="demonstração" src="./assets/img/demonstracao.png" />
+ </p>
+
+## 🌐 Site
+
+<https://hebert-santana.github.io/js-churrascometro/>
+
+## Autor
+~~~ javascript
+Hebert Santana
+~~~
+
+
+
---
+++ Churrascometro/assets/js/script.js
@@ -0,0 +1,90 @@
+// Carne        - 150gr por pessoa por hora     + de 6 horas 150gr por pessoa por hora
+// Cerveja      - 500ml por pessoa por hora     + de 6 horas 450ml por pessoa por hora
+// Refrigerante - 400ml por pessoa por hora     + de 6 horas 250ml por pessoa por hora
+// (Média) Adoslescente consome equivalente à 0,8 pessoa que não bebe.
+// (Média) Criança (menor de 12 anos) consome equivalente à 0,5 pessoa que não bebe.
+
+var horas = 5;
+var adolescente_equivalente = 0.8;
+var crianca_equivalente = 0.5;
+// criando variável (usando let) que vai receber o input
+let inputAdultos_b = document.getElementById("adultos_bebem");
+let inputAdultos_nb = document.getElementById("adultos_naobebem");
+let inputAdolescente = document.getElementById("menorde18");
+let inputCriancas = document.getElementById("criancas");
+
+let inputDuracao = document.getElementById("duracao");
+
+// imprimindo as variáveis, importante determinar com ".value" pra buscar o valor
+//console.log(inputAdultos.value, inputCriancas.value, inputDuracao.value);//
+
+let resultado = document.getElementById("resultado");
+
+// determinando a função
+
+function calcular() {
+    console.log("Calculando...");
+// determinar a quantidade de pessoas que serão inseridas .value
+    let adultos_b = inputAdultos_b.value;
+    let adultos_nb = inputAdultos_nb.value;
+    let adolescente = inputAdolescente.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+// função duracao * número de adultos + função duracao/2 * número de crianças 
+    let Total_Carne = (carnehoras(duracao) * (parseInt(adultos_b) + (parseInt(adultos_nb))) + (carnehoras(duracao) * adolescente_equivalente * adolescente) + (carnehoras(duracao) * crianca_equivalente * criancas));
+    let Total_Cerveja = cervejahoras(duracao) * adultos_b
+    let Total_Refri = (refrihoras(duracao) * adultos_nb) + (refrihoras(duracao) * adolescente_equivalente * adolescente) + (refrihoras(duracao) * crianca_equivalente * criancas);
+
+
+// PRIMEIRA FRASE //
+    resultado.innerHTML = `<p>Para o seu churrasco, você deve ter:</p>`
+
+
+// criando o RESULTADO "${quantidade}""
+    resultado.innerHTML += `<strong> <p>${(Total_Carne) / 1000}Kg de carne.</p> </strong>`
+    
+// += para não substituir o que já está em innerHTML
+// usar função pra pegar o maior inteiro após a divisão
+    resultado.innerHTML += `<strong> <p>${Math.ceil((Total_Cerveja) / 355)} latas de cerveja.</p> </strong>`    
+    resultado.innerHTML += `<strong> <p>${Math.ceil((Total_Refri) / 2000)} garrafas de 2 litros de refrigerante.</p> </strong>`
+    resultado.innerHTML += `<center> <p>BOA FESTA!</p> </center>`
+    
+
+
+}
+
+// função carne em relação as horas
+
+function carnehoras(duracao) {          // consumo médio sempre de 150g/h até 5 horas de duração
+    if (duracao >= horas){
+        return 120*(duracao - horas) + 150*horas;
+    }
+    else {
+        return 150*duracao;
+    }
+}
+
+// função cerveja em relação as horas
+
+function cervejahoras(duracao) {
+    if (duracao >= horas){
+        return ((450*(duracao - horas)) + 600*horas)  ; // 600*5 das 5 primeiras horas + 450 ml por hora
+    }
+    else {
+        return 600*duracao;
+    }
+}
+
+// função refrigerante em relação as horas
+
+function refrihoras(duracao) {
+    if (duracao >= horas){
+        return (( 250*(duracao - horas)) + 400*horas);
+    }
+    else {
+        return 400*duracao;
+    }
+}
+
+
